439dde0207e2912752c8cb720eb2ec57d0eb76b0
Sebastian Marines <sebastian0marines@gmail.com>
2022-07-09 21:01:54 -0500

    Create board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,54 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Square(props: { value: string; onClick: () => void }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
-export default App
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+
+  useEffect(() => {}, []);
+
+  let renderSquare = (i: number) => {
+    return (
+      <Square
+        value={board[i]}
+        onClick={() => {
+          setBoard(board.map((_, j) => (j === i ? "X" : board[j])));
+        }}
+      />
+    );
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <div>
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
